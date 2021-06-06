6cedfbe53f380d99f84d85086dcda580f979bd61
catamelne <catalinamelnesiuk@mail.com>
2021-06-06 11:23:25 -0300

    .

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,29 @@
 import './NavBar.css';
-import logo from '../../assets/images/logo.svg'
 import "bootstrap/dist/css/bootstrap.min.css"
 
+
+
+
+
 function NavBar() {
+
+  
   return (
 
     <nav>
-      <div class="container-fluid">
-       <div class="row">
-         <div class="col-md-6">
-            <a href="#" class="brand">MYTIENDAPP</a>
+      <div className="container-fluid">
+       <div className="row">
+         <div className="col-md-6">
+            <p href="#" className="brand">MYTIENDAPP</p>
          </div>
-         <div class="col-md-6">
-           <div class="main">
-            <a class="main-item" href="#">Inicio</a>
-            <a class="main-item"  href="#">Quienes somos</a>
-            <a class="main-item"  href="#">Contacto</a>
-            <a class="main-item"  href="#">Productos</a>
-            
-            </div>
+         <div className="col-md-6">
+           <ul className="main d-flex">
+            <li className="main-item">Inicio</li>
+            <li className="main-item">Quienes somos</li>
+            <li className="main-item">Contacto</li>
+            <li className="main-item">Productos</li>
+            <li className="main-item">Loging</li>
+            </ul>
          </div>
        </div>
       </div>
